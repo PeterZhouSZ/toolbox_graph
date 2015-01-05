818cdc8fea133e5744786737267d72235dd961ad
wang-ps <wangps@foxmail.com>
2015-01-05 18:20:25 +0800

    change plot_mesh

--- plot_mesh.m
+++ plot_mesh.m
@@ -22,12 +22,11 @@ end
 
 options.null = 0;
 
-name            = getoptions(options, 'name', '');
 normal          = getoptions(options, 'normal', []);
-face_color      = getoptions(options, 'face_color', .7);
-edge_color      = getoptions(options, 'edge_color', 0);
 normal_scaling  = getoptions(options, 'normal_scaling', .8);
-sanity_check = getoptions(options, 'sanity_check', 1);
+light = getoptions(options, 'light', false);
+face_vertex_color = getoptions(options, 'face_vertex_color',  ones(size(vertex))'*0.5); 
+
 
 if size(vertex,1)==2
     % 2D triangulation
@@ -94,41 +93,12 @@ end
 vertex = vertex';
 face = face';
 
-if strcmp(name, 'bunny') || strcmp(name, 'pieta')
-%    vertex = -vertex;
-end
-if strcmp(name, 'armadillo')
-    vertex(:,3) = -vertex(:,3);
-end
-
-if sanity_check && (size(face,2)~=3 || (size(vertex,2)~=3 && size(vertex,2)~=2))
-    error('face or vertex does not have correct format.');
-end
-
-if ~isfield(options, 'face_vertex_color') || isempty(options.face_vertex_color)
-    options.face_vertex_color = zeros(size(vertex,1),1);
-end
-face_vertex_color = options.face_vertex_color;
-
-
-if isempty(face_vertex_color)
-    h = patch('vertices',vertex,'faces',face,'facecolor',[face_color face_color face_color],'edgecolor',[edge_color edge_color edge_color]);
+if size(face_vertex_color,1)==size(vertex,1)
+    shading_type = 'interp';
 else
-    nverts = size(vertex,1);
-    % vertex_color = rand(nverts,1);
-    if size(face_vertex_color,1)==size(vertex,1)
-        shading_type = 'interp';
-    else
-        shading_type = 'flat';
-    end
-    h = patch('vertices',vertex,'faces',face,'FaceVertexCData',face_vertex_color, 'FaceColor',shading_type);
+    shading_type = 'flat';
 end
-colormap gray(256);
-lighting phong;
-% camlight infinite; 
-camproj('perspective');
-axis square; 
-axis off;
+h = patch('vertices',vertex,'faces',face,'FaceVertexCData',face_vertex_color, 'FaceColor',shading_type, 'EdgeColor', 'none');
 
 if ~isempty(normal)
     % plot the normals
@@ -140,44 +110,28 @@ if ~isempty(normal)
     hold off;
 end
 
-cameramenu;
-switch lower(name)
-    case 'hammerheadtriang'
-        view(150,-45);
-    case 'horse'
-        view(134,-61);
-    case 'skull'
-        view(21.5,-12);
-    case 'mushroom'
-        view(160,-75);
-    case 'bunny'
-%        view(0,-55);
-        view(0,90);
-    case 'david_head'
-        view(-100,10);
-    case 'screwdriver'
-        view(-10,25);
-    case 'pieta'
-        view(15,31);
-    case 'mannequin'
-        view(25,15);
-    case 'david-low'
-        view(40,3);
-    case 'brain'
-        view(30,40);
-    case 'pelvis'
-        view(5,-15);
-end
-view_param = getoptions(options, 'view_param', []);
-if not(isempty(view_param))
-    view(view_param(1),view_param(2));
-end
+% cameramenu;
+cameratoolbar('show')
+cameratoolbar('SetMode', 'orbit');
+cameratoolbar('SetCoordSys','none');
+ 
+    
+% colormap gray(256);
+lighting phong;
 
-axis tight;
+camproj('perspective');
+axis square; 
+axis off;
+% axis tight;
 axis equal;
-shading interp;
-camlight;
 
-if strcmp(name, 'david50kf') || strcmp(name, 'hand')
-    zoom(.85);
+if light || strcmp(shading_type, 'interp');
+    shading interp;
+    camlight;
+    camlight infinite; 
+    camlight(135, 60); camlight(225, 30);
+    camlight(180, -60); 
+    material([0.4 0.5 0.5])
 end
+
+
