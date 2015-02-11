d47e4750833a7e9a3869f0490d178f058d275901
wang-ps <wangps@foxmail.com>
2015-02-11 14:23:57 +0800

    Revert "Revert "optimize code""
    This reverts commit d4212a691fcdcd01f9281a1bf48388b8ed6d0d49.

--- compute_face_Laplacian.m
+++ compute_face_Laplacian.m
@@ -1,25 +1,27 @@
-function L = compute_face_Laplacian(pos, fring)
+function [L, W] = compute_face_Laplacian(pos, fring)
+% distance based face center laplacian
 % L must be normalized for filtering
+% diag(W)^-1 * L is the normalized laplacian
 
 nf = size(fring, 2);
 i = zeros(1, 4*nf);
 j = zeros(1, 4*nf);
 s = zeros(1, 4*nf);
-
-weight = zeros(3, 1);
+W = zeros(1, nf);
+weight = zeros(1, 3);
 for k = 1:nf	
 	for h = 1:3
 		k1 = fring{k}(h);
 		dp = pos(:, k1) - pos(:, k);
 		weight(h) = 1 / (dp'*dp)^0.5;
     end
-    weight = weight / sum(weight);
+    sw = sum(weight);
     
-    j(4*k-3 : 4*k) = k;
-	i(4*k-3 : 4*k) = [fring{k}, k];
-	s(4*k-3 : 4*k) = [-weight, 1];
+    i(4*k-3 : 4*k) = k;
+	j(4*k-3 : 4*k) = [fring{k}, k];
+	s(4*k-3 : 4*k) = [-weight, sw];
+    W(k) = sw;
 end
-
 L = sparse(i, j, s);
 end
     
