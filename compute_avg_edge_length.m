c2feaa3e2a10d726b6294bbabc812b2ec287dbd1
wang-ps <wangps@foxmail.com>
2015-03-03 14:35:43 +0800

    Optimize code

--- compute_avg_edge_length.m
+++ compute_avg_edge_length.m
@@ -1,14 +1,16 @@
-function [ avgEdge ] = compute_avg_edge_length( vertex, faces )
+function [ avgEdge ] = compute_avg_edge_length( vertex, face )
 %COMPUTE_AVG_EDGE_LENGHT compute the average edge length
 
-avgEdge = 0; 
-n = size(faces, 2);
-for i = 1:n
-    v1 = vertex(:, faces(1, i));
-    v2 = vertex(:, faces(2, i));
-    v3 = vertex(:, faces(3, i));
-    avgEdge = avgEdge + norm(v1-v2) + norm(v1-v3) + norm(v2-v3);
-end
+n = size(face, 2);
+
+v1 = vertex(:, face(1, :));
+v2 = vertex(:, face(2, :));
+v3 = vertex(:, face(3, :));
+
+avgEdge = sum( sqrt( sum((v1-v2).^2)) ) + ...
+          sum( sqrt( sum((v1-v3).^2)) ) + ...
+          sum( sqrt( sum((v3-v2).^2)) ) ;
+      
 avgEdge = avgEdge / (3*n);
 
 end
