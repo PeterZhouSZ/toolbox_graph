4996392007222a49f513f7ec6ceb383b2a1bbea4
wang-ps <wangps@foxmail.com>
2015-03-05 17:31:39 +0800

    optimize code

--- compute_rotation.m
+++ compute_rotation.m
@@ -8,9 +8,16 @@ function M = compute_rotation(v,alpha)
 %   Copyright (c) 2004 Gabriel Peyré
 
 v = reshape(v,3,1);
-v = v/norm(v,'fro');
 
-S = [0   -v(3) v(2);
+d = v'*v;
+if d < 1.0e-10
+    M = eye(3);    
+else
+    v = v/d^0.5;
+    S = [0   -v(3) v(2);
     v(3)   0  -v(1);
     -v(2)  v(1) 0];
-M = v*v' + cos(alpha)*(eye(3) - v*v') + sin(alpha)*S;
+    M = v*v' + cos(alpha)*(eye(3) - v*v') + sin(alpha)*S;
+end
+
+
