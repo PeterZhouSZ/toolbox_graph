bc11c07b88300bcc1ed2fef259c9ae592b3eb92f
Pengshuai Wang <wangps@foxmail.com>
2015-03-19 16:35:19 +0800

    Optimize code - edge_face_ring, triangulation2adjacency

--- compute_edge_face_ring.m
+++ compute_edge_face_ring.m
@@ -7,7 +7,7 @@ function e2f = compute_edge_face_ring(F)
 %   e2f(i,j) and e2f(j,i) are the number of the two faces adjacent to
 %   edge (i,j).
 
-
+nf = size(F, 2);
 i = [F(1,:) F(2,:) F(3,:)];
 j = [F(2,:) F(3,:) F(1,:)];
 s = [1:nf 1:nf 1:nf];
